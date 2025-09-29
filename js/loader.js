c99c2f9b6fd2b21efc737baec2a682b8d16933c2
Vikas Pondric <vikaspondric81@gmail.com>
2025-09-29 18:44:17 +0530

    update

---
+++ js/loader.js
@@ -0,0 +1,12 @@
+$(window).on("load", function() {
+
+		/* ----------------------------------------------------------- */
+		/*  PAGE PRELOADER
+        /* ----------------------------------------------------------- */
+		
+		var preloader = $('#preloader');
+		setTimeout(function() {
+			preloader.addClass('preloaded');
+		}, 800);
+
+	});
